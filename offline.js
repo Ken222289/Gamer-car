318dcb5c80865dc0c7b0047b7f1cccbb2466176c
Ken222289 <107600191+Ken222289@users.noreply.github.com>
2022-06-16 11:13:59 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1655351514,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/fran-sheet0.png",
+		"images/car1-sheet0.png",
+		"images/tree-sheet0.png",
+		"images/tree1-sheet0.png",
+		"images/sart1-sheet0.png",
+		"images/tnt-sheet0.png",
+		"images/gamerover1-sheet0.png",
+		"images/play-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
